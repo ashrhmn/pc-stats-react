a4ed6838262602c7e1186a0935f320db8036b26b
Ashik Rahman <43120823+ashrhmn@users.noreply.github.com>
2021-09-05 08:22:27 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    // purge: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
