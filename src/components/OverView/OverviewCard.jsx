6e9e3b64cebe02e6573c210a4aae3a1543b87b69
Ashik Rahman <ashrhmn99@gmail.com>
2021-09-06 03:00:35 +0600

    Change CSS

--- src/components/OverView/OverviewCard.jsx
+++ src/components/OverView/OverviewCard.jsx
@@ -4,7 +4,7 @@ function OverviewCard({ data, title }) {
   return (
     <div
       style={{ background: "#5E609C", minHeight: "180px" }}
-      className="p-4 rounded-xl text-white w-80"
+      className="p-4 rounded-xl text-white w-80 m-2"
     >
       <div className="text-center text-xl font-bold mb-2">{title}</div>
       <table className="table-auto w-full">
--- src/components/OverView/index.jsx
+++ src/components/OverView/index.jsx
@@ -5,7 +5,8 @@ import SystemCard from './SystemCard'
 
 function OverView() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-y-6 justify-items-center">
+        // <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-y-6 justify-items-center">
+        <div className="flex flex-row flex-wrap justify-around">
             <SystemCard/>
             <CpuCard/>
             <GraphicsCard/>
